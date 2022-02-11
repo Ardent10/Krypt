4820fef6d8926882af5ca3f1b590e1f860dfd0b8
Zakariya khan <zak786khan@gmail.com>
2022-02-11 23:37:06 +0530

    mostly

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import {SiEthereum} from "react-icons/si";
 import {BsInfoCircle} from "react-icons/bs";
 import { TransactionContext } from "../context/TransactionsContext";
 import {Loader} from "./";
-
+import { shortenAddress } from "../utils/shortenAddress";
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white"
 const Input = ({placeholder,name,type,value,handleChange}) =>(
@@ -76,7 +76,9 @@ const Welcome = ()=>{
 
                       </div>
                         <div >
-                            <p className="text-white font-light text-sm">Hex</p>
+                            <p className="text-white font-light text-sm">
+                              {currentAccount? shortenAddress(currentAccount) :"Hex"}
+                            </p>
                             <p className="text-white font-semibold text-lg mt-1">Ethereum</p>
                         </div>
                     </div>
